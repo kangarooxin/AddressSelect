b0cf48e721d2561264ee9dbc580a8f3f0e2308ba
pangxin <pangxin01822@pwrd.com>
2017-01-13 16:29:26 +0800

    update

--- AddressSelect/jquery.AddressSelect.js
+++ AddressSelect/jquery.AddressSelect.js
@@ -30,46 +30,40 @@
                 var $provinceSelect = $this.find(o.province);
                 var $citySelect = $this.find(o.city);
                 var $districtSelect = $this.find(o.district);
-                AddressSelect.buildSelect($countrySelect, 1, 0);
+                AddressSelect.buildSelect(o, $countrySelect, 1, 0);
 
                 if (o.country_val != undefined) {
                     $countrySelect.val(o.country_val);
-                    AddressSelect.buildSelect($provinceSelect, 2, o.country_val);
-                } else {
-                    AddressSelect.emptySelect($provinceSelect, 2);
                 }
+                AddressSelect.buildSelect(o, $provinceSelect, 2, o.country_val);
 
                 if (o.province_val != undefined) {
                     $provinceSelect.val(o.province_val);
-                    AddressSelect.buildSelect($citySelect, 3, o.province_val);
-                } else {
-                    AddressSelect.emptySelect($citySelect, 3);
                 }
+                AddressSelect.buildSelect(o, $citySelect, 3, o.province_val);
 
                 if (o.city_val != undefined) {
                     $citySelect.val(o.city_val);
-                    AddressSelect.buildSelect($districtSelect, 4, o.city_val);
-                } else {
-                    AddressSelect.emptySelect($districtSelect, 4);
                 }
+                AddressSelect.buildSelect(o, $districtSelect, 4, o.city_val);
 
                 if (o.district_val != undefined) {
                     $districtSelect.val(o.district_val);
                 }
 
-                AddressSelect.bindChange($countrySelect, o.callback, $provinceSelect, 2);
-                AddressSelect.bindChange($provinceSelect, o.callback, $citySelect, 3);
-                AddressSelect.bindChange($citySelect, o.callback, $districtSelect, 4);
-                AddressSelect.bindChange($districtSelect, o.callback);
+                AddressSelect.bindChange(o, $countrySelect, o.callback, $provinceSelect, 2);
+                AddressSelect.bindChange(o, $provinceSelect, o.callback, $citySelect, 3);
+                AddressSelect.bindChange(o, $citySelect, o.callback, $districtSelect, 4);
+                AddressSelect.bindChange(o, $districtSelect, o.callback);
             });
         }
     };
 
-    AddressSelect.bindChange = function($select, callback, $subSelect, type) {
+    AddressSelect.bindChange = function(opts, $select, callback, $subSelect, type) {
         $select.change(function() {
             var val = parseInt($(this).val());
             if($subSelect) {
-                AddressSelect.buildSelect($subSelect, type, val);
+                AddressSelect.buildSelect(opts, $subSelect, type, val);
             }
             if (typeof callback == 'function') {
                 callback(AddressSelect.getData(val));
@@ -79,15 +73,17 @@
 
     var emptyAddress = ['国家', '省份/自治区', '城市/地区/自治州', '区/县'];
     AddressSelect.emptySelect = function($select, type) {
-        $select.empty();
-        $select.append('<option value="0">' + emptyAddress[type - 1] + '</option>');
+        $select.empty().append('<option value="0">' + emptyAddress[type - 1] + '</option>').hide();
     };
 
-    AddressSelect.buildSelect = function($select, type, pid, defVal) {
+    AddressSelect.buildSelect = function(opts, $select, type, pid, defVal) {
         if($select.length == 0) return;
         var oldVal = $select.val();
         AddressSelect.emptySelect($select, type);
         var subAreas = _.where(address_data, { parent_id: parseInt(pid), type: type });
+        if(opts.always_show || subAreas.length > 0) {
+            $select.show();
+        }
         subAreas = _.sortBy(subAreas, 'id');
         _.each(subAreas, function(area) {
             $select.append('<option value="' + area.id + '">' + area.name + '</option>');
@@ -95,9 +91,7 @@
         if (defVal > 0) {
             $select.val(defVal);
         }
-        if (defVal > 0 || oldVal > 0) {
-            $select.trigger('change');
-        }
+        $select.trigger('change');
     };
 
     AddressSelect.getData = function(id) {
@@ -161,6 +155,7 @@
         province_val: undefined,
         city_val: undefined,
         district_val: undefined,
+        always_show: false,
         callback: function(data) {}
     };
 })(jQuery, _);
